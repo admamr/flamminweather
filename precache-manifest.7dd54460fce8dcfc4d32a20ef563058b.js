eea12907f590b7c3cf4f6e3895b5a66259dbed1c
admamr <71948519+admamr@users.noreply.github.com>
2025-03-14 13:21:09 +0200

    Updates

---
+++ precache-manifest.7dd54460fce8dcfc4d32a20ef563058b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c957dbcd26b40e97bcc4683a885b0d",
+    "url": "/flamminweather/index.html"
+  },
+  {
+    "revision": "6be81036a9b8ec65322e",
+    "url": "/flamminweather/static/css/main.848f5000.chunk.css"
+  },
+  {
+    "revision": "3da65bc6c56c819f3e6b",
+    "url": "/flamminweather/static/js/2.d78f55af.chunk.js"
+  },
+  {
+    "revision": "4cb447811eafcde62d61d67183d8b566",
+    "url": "/flamminweather/static/js/2.d78f55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb16624479dcd8fc855d",
+    "url": "/flamminweather/static/js/3.659da4c5.chunk.js"
+  },
+  {
+    "revision": "6be81036a9b8ec65322e",
+    "url": "/flamminweather/static/js/main.29531994.chunk.js"
+  },
+  {
+    "revision": "eeed0fa5a24cc05ed5f7",
+    "url": "/flamminweather/static/js/runtime-main.202e2ab9.js"
+  }
+]);
